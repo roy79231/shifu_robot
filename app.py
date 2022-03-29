72cd99ac8b1b70cb97053eeed1e445d3249e3992
Roy Yang <roy82859370@gmail.com>
2022-03-30 00:23:29 +0800

    beta_1

--- app.py
+++ app.py
@@ -42,7 +42,11 @@ def handle_message(event):
     r = '不好意思，請依照格式輸入，謝謝!'
     x = random.randint(20,80)
     y = x + 20
-    if '1-1' in msg :
+    if '陳立軒' in msg and '1-1' in msg :
+        r = '郁達師傅掐指一算，你這次告白一定成功。'
+    elif '陳立軒' in msg and '1-2' in msg :
+        r = '郁達師傅有言:在您出生當日執行，會有最高機率成功。'    
+    elif '1-1' in msg :
         r = '你有',x,'%的機率會成功，師傅有言：這只是您目前的命運，您依舊能靠你後天努力來提高機率。'
     elif '1-2' in msg :
         r = '郁達師傅說您最適合在下個重要節慶告白,成功機率有到', y ,'%。'
